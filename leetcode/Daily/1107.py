028d8c27b93fa75ce91f645355b4a6c3f1bba446
BennyJane <3355817143@qq.com>
2021-12-15 22:17:14 +0800

    更新题目

--- leetcode/Daily/1107.py
+++ leetcode/Daily/1107.py
@@ -162,21 +162,19 @@ class Solution4:
 # 851. 喧闹和富有
 class Solution5:
     def loudAndRich(self, richer: List[List[int]], quiet: List[int]) -> List[int]:
-        d = defaultdict(list)
-        n = len(quiet)
-        ans = [i for i in range(n)]
+        N = len(quiet)
+        edges = [[] for _ in range(N)]
+        for k, v in richer:
+            edges[v].append(k)
+        memorize = [None] * N
 
-        for x, y in richer:
-            d[y].append(x)
+        def dfs(node):
+            if memorize[node] is None:
+                memorize[node] = node
+                for i in edges[node]:
+                    cand = dfs(i)
+                    if quiet[cand] < quiet[memorize[node]]:
+                        memorize[node] = cand
+            return memorize[node]
 
-        # FIXME 深度优先搜索，查找所有满足钱不少于当前索引处x的列表
-        for i in range(n):
-            pass
-
-        for i in range(n):
-            res = i
-            for larger in d[i]:
-                if quiet[larger] < quiet[res]:
-                    res = larger
-            ans[i] = res
-        return ans
+        return map(dfs, range(N))
